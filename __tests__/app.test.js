bef92139a9a3f4152df21441acfdfe785020ecae
Sam Adams <sam.adams7785@gmail.com>
2022-11-03 11:05:39 -0700

    initial blocks data and block test

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,15 +1,23 @@
 const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
-// const request = require('supertest');
-// const app = require('../lib/app');
+const request = require('supertest');
+const app = require('../lib/app');
+
+const { blocks } = require('../lib/blocks-data');
 
 describe('backend-express-template routes', () => {
   beforeEach(() => {
     return setup(pool);
   });
-  it('example test - delete me!', () => {
-    expect(1).toEqual(1);
+
+  it('/blocks should return a list of blocks', async () => {
+    const res = await request(app).get('/blocks');
+    const expected = blocks.map((block) => {
+      return { id: block.id, name: block.name };
+    });
+    expect(res.body).toEqual(expected);
   });
+
   afterAll(() => {
     pool.end();
   });
